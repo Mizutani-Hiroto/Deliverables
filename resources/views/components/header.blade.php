806664f65bc08e6d30aee07a049d7c55319e3064
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2020-12-21 15:44:28 +0000

    create workmodel

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -28,7 +28,7 @@
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="〇〇で検索できます。" aria-label="Search">
+      <input class="form-control mr-sm-2" type="search" placeholder="〇〇で検索できます" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">検索する</button>
     </form>
   </div>
