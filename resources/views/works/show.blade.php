d5477f9a138823d4f41e4943c94ecd768e86a78d
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2021-02-27 14:43:15 +0000

    edit show

--- resources/views/works/show.blade.php
+++ resources/views/works/show.blade.php
@@ -12,7 +12,7 @@
       @else
     　<h3>作品名未定</h3>
     　@endif
-      <div class="row">
+      <div class="row text-center">
         <div class="col-sm-4">
           <p>説明</p>
         </div>
@@ -20,7 +20,7 @@
           <p>{{ $work->description }}</p>
         </div>
       </div>
-      <div class="row">
+      <div class="row text-center">
         <div class="col-sm-4">
           <p>サイズ</p>
         </div>
@@ -28,7 +28,7 @@
           <p>{{ $work->size }}</p>
         </div>
       </div>
-      <div class="row">
+      <div class="row text-center">
         <div class="col-sm-4">
           <p>ジャンル</p>
         </div>
@@ -36,7 +36,7 @@
           <p>{{ $work->genre }}</p>
         </div>
       </div>
-      <div class="row">
+      <div class="row text-center">
         <div class="col-sm-4">
           <p>製作期間</p>
         </div>
@@ -60,7 +60,7 @@
               
               <hr>
               
-              <h3>Evaluation</h3>
+              <h3>講評する</h3>
               @foreach($comments as $comment)
               <div class="card mb-3">
                 <div class="row">
@@ -73,7 +73,7 @@
                 <form method="POST" action="/works/{{ $work->id }}">
                   {{ csrf_field() }}
                   <textarea name="text" class="form-control"></textarea>
-                  <button type="submit">Evaluate</button>
+                  <button class="text-end"  type="submit">送信する</button>
                 </form>
               </div>
 
