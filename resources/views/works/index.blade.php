e06eb0cac5a08ad4d62217ce2a12f0cb2d17cc1f
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2021-04-05 12:18:59 +0000

    pacage.jsonの編集

--- resources/views/works/index.blade.php
+++ resources/views/works/index.blade.php
@@ -2,21 +2,18 @@
 
 @section('content')
 
-  <div class="content">
-    @include('components.header')
-    <div class="container">
-    <div class="row">
-      @foreach($works as $work)
-      <div class="col-md-3 col-sm-4 col-xs-6">
-        <a href="/works/{{ $work->id }}">
-          <img src="{{ $work->image_path }}" class="img-responsive list-item">
-        </a>
+@include('components.header')
+    <div class="container main">
+      <div class="row">
+        @foreach($works as $work)
+          <div class="col-md-3 col-sm-4 col-xs-6">
+            <a href="/works/{{ $work->id }}">
+            <img src="{{ $work->image_path }}" class="img-responsive list-item">
+            </a>
+          </div>
+        @endforeach
       </div>
-      @endforeach
     </div>
-  </div>
-  </div>
+
 
 @endsection
-
-<script src="{{ asset('js/image.js') }}"></script>
